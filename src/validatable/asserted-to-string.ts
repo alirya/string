f73393f9bf1d2a14c49e4f8c8cbf9334a49f4535
dikac <rigel234@yahoo.com>
2022-01-31 21:29:13 +0700

    add asserted to string

---
+++ src/validatable/asserted-to-string.ts
@@ -0,0 +1,16 @@
+import Validatable from "@alirya/validator/validatable/validatable";
+import ToString from "../to-string";
+import AssertValid from "@alirya/validator/validatable/assert/valid";
+
+export default function AssertedToString<ValidatableType extends Validatable<string>>(validatable : ValidatableType) : ValidatableType & ToString<[]> {
+
+    validatable.toString = function () : string {
+
+        AssertValid(this);
+
+        return validatable.value;
+    };
+
+    return validatable;
+
+}
