c488cf03316c0e73b906f5a17ef442c084fa7424
dikac <rigel234@yahoo.com>
2022-04-17 03:02:43 +0700

    fix type, bump version

--- src/validator/alphanumeric-void.ts
+++ src/validator/alphanumeric-void.ts
@@ -2,7 +2,7 @@ import Validator from '@alirya/validator/simple';
 import Instance from '@alirya/validator/validatable/validatable';
 import AlphanumericParameter from './alphanumeric-parameter';
 
-export default function AlphanumericVoid() : Validator<string, '', Readonly<Instance<'', string>>> {
+export default function AlphanumericVoid() : Validator<string, string, Readonly<Instance<string, string>>>{
 
     return AlphanumericParameter();
 }
--- src/validator/digit-void.ts
+++ src/validator/digit-void.ts
@@ -2,7 +2,7 @@ import Validator from '@alirya/validator/simple';
 import Instance from '@alirya/validator/validatable/validatable';
 import DigitParameter from './digit-parameter';
 
-export default function DigitVoid() : Validator<string, '', Readonly<Instance<'', string>>> {
+export default function DigitVoid() : Validator<string, string, Readonly<Instance<string, string>>> {
 
     return DigitParameter();
 }
