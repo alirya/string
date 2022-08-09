25b15c4f1b2bdfe70ecbe1f9648036d206c7d0ee
dikac <rigel234@yahoo.com>
2022-08-09 15:28:53 +0700

    fix: fix numeric type

--- src/ensure/numeric.ts
+++ src/ensure/numeric.ts
@@ -2,9 +2,9 @@ import AssertNumeric from '../assert/numeric';
 import NumericError from '../assert/throwable/numeric';
 
 export default function Numeric(
-    value : string,
+    value : string|number,
     error : (value:unknown)=>Error = NumericError.Parameters
-) : string {
+) : string|number {
 
     AssertNumeric(value, error);
 
